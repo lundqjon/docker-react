65513d2f006f28dda29b8a520a6612905593d307
lundqjon <jonas.lundqvist@tietoevry.com>
2025-05-09 08:41:28 +0200

    more testing #10

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React!!!!!!!!!YeahLoveIt!!!!
+          Learn React!!!!!!!!!YeahLoveItOr!!!!
         </a>
       </header>
     </div>
